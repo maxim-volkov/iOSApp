e023466f4466bd56436da8df7f8ab59f980eef17
HOGGISH <dangerhoggish@gmail.com>
2021-05-15 13:01:30 +0500

    new btn creation added

--- iOSApp/Resources/Extensions/UIButton+extensions.swift
+++ iOSApp/Resources/Extensions/UIButton+extensions.swift
@@ -9,23 +9,30 @@ import UIKit
 
 extension UIButton {
     
-    static func makeButton(title: String?, backgroundColor: UIColor?, borderColor: UIColor? = nil) -> UIButton {
+    static func makeButton(title: String?, backgroundColor: UIColor?, borderColor: UIColor? = nil, titleFont: UIFont = appMediumFont(size: 18), cornerRadius: CGFloat = 8) -> UIButton {
         let btn = UIButton()
         btn.setTitle(title, for: .normal)
         btn.backgroundColor = backgroundColor
-        btn.titleLabel?.font = appBoldFont(size: 18)
+        btn.titleLabel?.font = titleFont
         if let borderColor = borderColor {
             btn.setTitleColor(borderColor, for: .normal)
             btn.layer.borderWidth = 1
             btn.layer.borderColor = borderColor.cgColor
         }
-        btn.layer.cornerRadius = 8
+        btn.layer.cornerRadius = cornerRadius
         btn.snp.makeConstraints { make in
             make.height.equalTo(appBtnHeight)
         }
         return btn
     }
     
+    static func makeLabelButton(title: String, textColor: UIColor?, titleFont: UIFont = appMediumFont(size: 18)) -> UIButton {
+        let btn = UIButton()
+        btn.setTitle(title, for: .normal)
+        btn.setTitleColor(textColor, for: .normal)
+        btn.titleLabel?.font = titleFont
+        return btn
+    }
 }
 
 
