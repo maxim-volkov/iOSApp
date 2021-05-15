150b33e19478f97eb84282ad9a29351cf8512940
HOGGISH <dangerhoggish@gmail.com>
2021-05-15 22:51:55 +0500

    new textfield added one TimeTextfield

--- iOSApp/Signing/Confirmation/ConfirmViewController.swift
+++ iOSApp/Signing/Confirmation/ConfirmViewController.swift
@@ -7,17 +7,17 @@
 
 import UIKit
 
-final class ConfirmViewController: BaseViewController {
-
+final class ConfirmViewController: BaseViewController, OneTimeCodeTextFieldDelegate {
+    
     var viewModel: ConfirmViewModel!
     private let scrollView = UIScrollView()
-    private let stackView = UIStackView(axis: .vertical, distribution: .fillProportionally, spacing: 20)
-
+    private let stackView = UIStackView(axis: .vertical, distribution: .fill, spacing: 20)
+    
     private lazy var confirmationLabel = UILabel.makeLabel(RL.sentConfirmationCode(viewModel.emailOrPhoneNumber), font: appMediumFont(size: 16), color: .appGrayWhiteColor, textAligment: .center, numberOfLines: 0)
-    private let textField = AppTextField(placeholder: RL.enterCode())
+    private lazy var textField = OneTimeCodeTextField(with: viewModel.codeCount)
     private let didNotRecieveCode = UIButton.makeLabelButton(title: RL.notReceivedCode(), textColor: appBrandColor, titleFont: appMediumFont(size: 16))
     private let nextBtn = UIButton.makeButton(title: RL.next(), backgroundColor: .appBlackBrandColor)
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
@@ -25,6 +25,7 @@ final class ConfirmViewController: BaseViewController {
     }
     
     private func setupView() {
+        textField.oneTimeCodeTextFieldDelegate = self
         nextBtn.addTarget(self, action: #selector(nextBtnTapped), for: .touchUpInside)
         didNotRecieveCode.addTarget(self, action: #selector(didNotRecieveCodeTapped), for: .touchUpInside)
         view.addSubview(scrollView)
@@ -42,14 +43,16 @@ final class ConfirmViewController: BaseViewController {
             make.top.equalToSuperview().inset(appExtraLargePadding)
             make.leading.width.trailing.equalToSuperview().inset(appLargePadding)
         }
-
+        textField.snp.makeConstraints { make in
+            make.height.equalTo(60)
+        }
         stackView.snp.makeConstraints { make in
             make.top.equalTo(confirmationLabel.snp.bottom).inset(-appExtraLargePadding)
             make.leading.width.trailing.equalToSuperview()
         }
     }
     @objc private func nextBtnTapped() {
-        if !viewModel.nextBtnTapped() {
+        if !viewModel.nextBtnTapped(code: textField.text) {
             if let aShake = makeShakeAnimation() {
                 scrollView.layer.add(aShake, forKey: "shake")
             }
@@ -60,4 +63,15 @@ final class ConfirmViewController: BaseViewController {
             textField.text = ""
         }
     }
+    
+    //MARK:- OneTimeCodeTextFieldDelegate
+    func didEnterLastDigit(_ text: String) {
+        view.endEditing(true)
+        if !viewModel.nextBtnTapped(code: textField.text) {
+            if let aShake = makeShakeAnimation() {
+                scrollView.layer.add(aShake, forKey: "shake")
+            }
+        }
+    }
+    
 }
--- iOSApp/Signing/Confirmation/ConfirmViewModel.swift
+++ iOSApp/Signing/Confirmation/ConfirmViewModel.swift
@@ -10,14 +10,16 @@ import Foundation
 final class ConfirmViewModel {
     
     private let parentCoordinator: ConfirmCoordinator
+    private(set) var codeCount: Int = 6
     private(set) var emailOrPhoneNumber: String
     init(coordinator: Coordinator, emailOrPhoneNumber: String) {
         self.parentCoordinator = coordinator as! ConfirmCoordinator
         self.emailOrPhoneNumber = emailOrPhoneNumber
     }
     
-    func nextBtnTapped() -> Bool {
-        return false
+    func nextBtnTapped(code: String?) -> Bool {
+        guard let code = code?.trim(), code.count > 6 else { return false }
+        return true
     }
     
     func didNotRecieveCodeTapped() -> Bool {
