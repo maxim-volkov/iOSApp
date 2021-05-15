a692d26c64df2ac0bb319e1035844a5b67f30545
HOGGISH <dangerhoggish@gmail.com>
2021-05-15 22:51:38 +0500

    code text field added for seperate labelled text

---
+++ iOSApp/Resources/Customs/ConfirmationCodeTextField.swift
@@ -0,0 +1,105 @@
+//
+//  ConfirmationCodeTextField.swift
+//  iOSApp
+//
+//  Created by HOGGISH on 15/05/21.
+//
+
+import UIKit
+
+protocol OneTimeCodeTextFieldDelegate {
+    func didEnterLastDigit(_ text: String)
+}
+
+final class OneTimeCodeTextField: UITextField {
+    
+    var oneTimeCodeTextFieldDelegate: OneTimeCodeTextFieldDelegate!
+    
+    private var digitLabels = [UILabel]()
+    
+    private lazy var tapRecognizer: UITapGestureRecognizer = {
+        let recognizer = UITapGestureRecognizer()
+        recognizer.addTarget(self, action: #selector(becomeFirstResponder))
+        return recognizer
+    }()
+    
+    init(with slotCount: Int = 6) {
+        super.init(frame: .zero)
+        
+        configureTextField()
+        let labelsStackView = createLabelsStackView(with: slotCount)
+        addSubview(labelsStackView)
+        
+        addGestureRecognizer(tapRecognizer)
+        labelsStackView.snp.makeConstraints { (make) in
+            make.top.bottom.leading.trailing.equalToSuperview()
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    private func configureTextField() {
+        tintColor = .clear
+        textColor = .clear
+        keyboardType = .numberPad
+        textContentType = .oneTimeCode
+        
+        addTarget(self, action: #selector(textDidChange), for: .editingChanged)
+        delegate = self
+    }
+    
+    private func createLabelsStackView(with count: Int) -> UIStackView {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.alignment = .fill
+        stackView.distribution = .fillEqually
+        stackView.spacing = 8
+        
+        for _ in 1 ... count {
+            let label = UILabel()
+            label.translatesAutoresizingMaskIntoConstraints = false
+            label.textAlignment = .center
+            label.font = .systemFont(ofSize: 40)
+            label.isUserInteractionEnabled = true
+            label.layer.borderWidth = 1
+            label.layer.borderColor = appGrayColor.cgColor
+            label.layer.cornerRadius = 8
+            label.layer.masksToBounds = true
+            stackView.addArrangedSubview(label)
+            digitLabels.append(label)
+        }
+        
+        return stackView
+    }
+    
+    @objc private func textDidChange() {
+        
+        guard let text = self.text, text.count <= digitLabels.count else { return }
+        
+        for i in 0 ..< digitLabels.count {
+            let currentLabel = digitLabels[i]
+            
+            if i < text.count {
+                let index = text.index(text.startIndex, offsetBy: i)
+                currentLabel.text = String(text[index])
+            } else {
+                currentLabel.text?.removeAll()
+            }
+        }
+        
+        if text.count == digitLabels.count {
+            oneTimeCodeTextFieldDelegate.didEnterLastDigit(text)
+        }
+    }
+    
+}
+
+extension OneTimeCodeTextField: UITextFieldDelegate {
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        guard let characterCount = textField.text?.count else { return false }
+        return characterCount < digitLabels.count || string == ""
+    }
+}
