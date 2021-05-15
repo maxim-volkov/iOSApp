7722cf449086231ad0eb2a37b9e1c2c117476247
HOGGISH <dangerhoggish@gmail.com>
2021-05-15 12:59:47 +0500

    signUp UI changed

--- iOSApp/Signing/SignUp/SignUpViewController.swift
+++ iOSApp/Signing/SignUp/SignUpViewController.swift
@@ -11,39 +11,26 @@ final class SignUpViewController: BaseViewController {
 
     var viewModel: SignUpViewModel!
     private let scrollView = UIScrollView()
+    private let signUpToAppLabel = UILabel.makeLabel(RL.signUpToApp(), font: appBoldFont(size: 35), color: appDarkColor, textAligment: .center, numberOfLines: 3)
     private let stackView = UIStackView(axis: .vertical, distribution: .fillProportionally, spacing: 20)
 
-    private let emailTextField = AppTextField(placeholder: RL.emailPhoneNumber())
+    private lazy var emailPhoneTextField = AppTextField(placeholder: viewModel.title)
     private let passwordTextField = AppTextField(placeholder: RL.password(), isSecureTextEntry:  true)
     private let confirmPasswordTextField = AppTextField(placeholder: RL.confirmPassword(), isSecureTextEntry: true)
-    private let signUpBtn = UIButton.makeButton(title: RL.signUp(), backgroundColor: appBrandColor)
-    
+    private let signUpBtn = UIButton.makeButton(title: RL.signUp(), backgroundColor: appDarkColor)
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
         setupLayout()
     }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        navigationController?.navigationBar.prefersLargeTitles = true
-        let textAttributes = [NSAttributedString.Key.foregroundColor: appBrandColor]
-        navigationController?.navigationBar.titleTextAttributes = textAttributes
-        navigationController?.navigationBar.largeTitleTextAttributes = textAttributes
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        navigationController?.navigationBar.titleTextAttributes = nil
-        navigationController?.navigationBar.largeTitleTextAttributes = nil
-    }
+
     private func setupView() {
-        title = viewModel.title
         signUpBtn.addTarget(self, action: #selector(signUpBtnTapped), for: .touchUpInside)
         view.addSubview(scrollView)
-        scrollView.addSubview(stackView)
+        scrollView.addSubviews(signUpToAppLabel, stackView)
         stackView.alignment = .fill
-        stackView.addArrangedViews(emailTextField, passwordTextField, confirmPasswordTextField, signUpBtn)
+        stackView.addArrangedViews(emailPhoneTextField, passwordTextField, confirmPasswordTextField, signUpBtn)
     }
     
     private func setupLayout() {
@@ -51,12 +38,16 @@ final class SignUpViewController: BaseViewController {
             make.top.bottom.equalTo(view.safeAreaLayoutGuide)
             make.leading.trailing.equalToSuperview().inset(appLargePadding)
         }
+        signUpToAppLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().inset(appExtraLargePadding)
+            make.leading.width.trailing.equalToSuperview().inset(appLargePadding)
+        }
         stackView.snp.makeConstraints { make in
-            make.top.equalToSuperview().inset(appLargePadding)
+            make.top.equalTo(signUpToAppLabel.snp.bottom).inset(-appExtraLargePadding)
             make.leading.width.trailing.equalToSuperview()
         }
     }
     @objc private func signUpBtnTapped() {
-        viewModel.signUpBtnTapped(email: emailTextField.text, password: passwordTextField.text, confirmPassword: confirmPasswordTextField.text)
+        viewModel.signUpBtnTapped(email: emailPhoneTextField.text, password: passwordTextField.text, confirmPassword: confirmPasswordTextField.text)
     }
 }
